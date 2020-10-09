2696534583ac90c4ebf34ac94bf609c65f6afa55
Mitsuya Watanabe <mitsuya.watanabe85@gmail.com>
2020-10-09 19:03:11 +0200

    Fix import path setting in jest.

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,9 @@ module.exports = {
     "^.+\\.js$": "./node_modules/babel-jest",
     "^.+\\.vue$": "vue-jest",
   },
-
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
   moduleFileExtensions: ["js", "vue"],
   moduleDirectories: ["<rootDir>/node_modules", "<rootDir>/src/js"],
 };
