e341a17eb157a768f631986e55fb77432b7fb473
Mitsuya Watanabe <mitsuya.watanabe85@gmail.com>
2020-10-10 14:29:54 +0200

    Add settings layout.

--- src/pages/Settings.vue
+++ src/pages/Settings.vue
@@ -1,5 +1,19 @@
 <template>
   <div>
-    <h1>Settings</h1>
+    <StaticEditName :name="'Mitsuya Watanabe'" />
+    <div><span>Email: </span>mitsuya.watanabe@gmail.com</div>
+    <div><span>Birth: </span>06.08.1985</div>
   </div>
 </template>
+
+<script>
+import StaticEditName from "@/components/StaticEditName";
+import UserAccountForm from "@/components/UserAccountForm";
+
+export default {
+  components: {
+    StaticEditName,
+    UserAccountForm,
+  },
+};
+</script>
